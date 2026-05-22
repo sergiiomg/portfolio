160e0ecca44c804ed46749749ffae27866db7d30
Sergiio <sergiiomgdaw@gmail.com>
2026-05-22 13:36:44 +0200

    Crear rutas y layout base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ProjectDetailComponent } from './features/project-detail/project-detail.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'proyectos',
+        component: ProjectsComponent
+    },
+    {
+        path: 'proyectos/:slug',
+        component: ProjectDetailComponent
+    },
+    {
+        path: 'experiencia',
+        component: ExperienceComponent
+    },
+    {
+        path: 'contacto',
+        component: ContactComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+];
